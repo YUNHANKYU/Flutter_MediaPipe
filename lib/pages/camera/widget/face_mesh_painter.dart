f81136e2d4e36af2f095094e0147d5b4f5a5cf47
YUNHANKYU <bhgt4563@gmail.com>
2022-06-23 08:27:03 +0900

    feat: 눈 좌우 양끝점을 이은 선과 입술 상하 양 끝 점을 이은 선 표시

--- lib/pages/camera/widget/face_mesh_painter.dart
+++ lib/pages/camera/widget/face_mesh_painter.dart
@@ -23,16 +23,21 @@ class FaceMeshPainter extends CustomPainter {
         ..strokeWidth = 4;
       var paint3 = Paint()
         ..color = Colors.blue
-        ..strokeWidth = 10;
+        ..strokeWidth = 6;
+      var paint4 = Paint()
+        ..color = Colors.green
+        ..strokeWidth = 4;
 
-      var redPoints = points.sublist(10, 20);
+      var redPoints = points.sublist(0, 1);
       var yellowPoints = points;
       var bluePoints = [points[5], points[15]];
+      var greenPoints = [points[134], points[205]];
 
-      print('최대: ${bluePoints[1]}');
-      print('최소: ${bluePoints[0]}');
-
-      print('길이: ${(bluePoints[1] - bluePoints[0]).distance}');
+      // print('최대: ${greenPoints[1]}');
+      // print('최소: ${greenPoints[0]}');
+      var eyeSize = (greenPoints[1] - greenPoints[0]).distance;
+      var lipSize = (bluePoints[1] - bluePoints[0]).distance;
+      print('길이: $eyeSize || 입술: $lipSize || 비율: ${lipSize / eyeSize}');
 
       canvas.drawPoints(PointMode.points,
           yellowPoints.map((point) => point * ratio).toList(), paint1);
@@ -40,10 +45,17 @@ class FaceMeshPainter extends CustomPainter {
       canvas.drawPoints(PointMode.points,
           redPoints.map((point) => point * ratio).toList(), paint2);
 
+      // 입 위 to 아래
       canvas.drawPoints(PointMode.points,
           bluePoints.map((point) => point * ratio).toList(), paint3);
 
       canvas.drawLine(bluePoints[0] * ratio, bluePoints[1] * ratio, paint3);
+
+      // 머리 위 to 아래
+      canvas.drawPoints(PointMode.points,
+          greenPoints.map((point) => point * ratio).toList(), paint4);
+
+      canvas.drawLine(greenPoints[0] * ratio, greenPoints[1] * ratio, paint4);
     }
   }
 
